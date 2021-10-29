6cff1df495094f05b8ecdeecedb2de8c6be583ee
Eelis Takala <eelis.takala@gmail.com>
2021-10-29 23:53:36 +0200

    Test launching jupyter notebook from lua script

--- lua/jupyter-nvim/init.lua
+++ lua/jupyter-nvim/init.lua
@@ -69,8 +69,38 @@ local function test()
 	createFloatingWindow()
 end
 
+local function read_process(process_file_handle)
+	local output = process_file_handle:read('*all')
+	print(output)
+end
+
+local function start_jupyter_notebook()
+	local cmd = '/home/eetakala/miniconda3/bin/jupyter notebook /home/eetakala/test/jupyter/test.sync.ipynb 2>&1'
+	jupyter_notebook_start = assert(io.popen(cmd))
+	-- os.execute("/home/eetakala/miniconda3/bin/jupyter notebook")
+end
+
+local function stop_jupyter_notebook()
+	-- jupyter_notebook_stop = assert(io.popen('/home/eetakala/miniconda3/bin/jupyter notebook stop'))
+	-- jupyter_notebook_stop:close()
+	-- jupyter_notebook_start:close()
+	os.execute("/home/eetakala/miniconda3/bin/jupyter notebook stop")
+end
+
+local function create_jupytext_pair(ipynb_name)
+	pair = assert(io.popen("/bin/cp /home/eetakala/test/jupyter/test.ipynb /home/eetakala/test/jupyter/test.sync.ipynb"))
+	read_process(pair)
+	pair = assert(io.popen("/home/eetakala/miniconda3/bin/jupytext --to py:percent /home/eetakala/test/jupyter/test.sync.ipynb"))
+	read_process(pair)
+	-- os.execute("/home/eetakala/miniconda3/bin/jupyter notebook")
+end
+
 return {
 	test = test,
 	createFloatingWindow = createFloatingWindow,
-	onResize = onResize
+	onResize = onResize,
+	start_jupyter_notebook = start_jupyter_notebook,
+	read_jupyter_notebook = read_jupyter_notebook,
+	stop_jupyter_notebook = stop_jupyter_notebook,
+	create_jupytext_pair = create_jupytext_pair
 }
